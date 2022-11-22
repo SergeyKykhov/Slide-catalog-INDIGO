f48800a2f6c661d17353a62ce0180fe7c1d5987e
Sergey Kykhov <ghrle@mail.ru>
2022-11-22 14:18:26 +0300

    Initial Commit

---
+++ Slide catalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slide catalog
+//
+//  Created by Sergey Kykhov on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
